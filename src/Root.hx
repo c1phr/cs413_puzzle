b004bdfe364bc926c8c95e10e74c6287a3536915
Ryan Batchelder <rb688@nau.edu>
2015-02-12 20:08:59 -0700

    Added new loading screen Added spritesheet and changed assetmanager to reflect that Moved some things around to prep for submission Gitignore TexturePacker projects

--- src/Root.hx
+++ src/Root.hx
@@ -20,29 +20,9 @@ class Root extends Sprite {
 
     public function start(startup:Startup) {
         assets = new AssetManager();
-	
-		assets.enqueue("assets/dot.png");
-		assets.enqueue("assets/floor2.png");
-		assets.enqueue("assets/dark.png");
-        assets.enqueue("assets/walls/eastWall.png");
-        assets.enqueue("assets/walls/westWall.png");
-        assets.enqueue("assets/walls/northWall.png");
-        assets.enqueue("assets/walls/southWall.png");
-        assets.enqueue("assets/title_font.png");
         assets.enqueue("assets/title_font.fnt");
-        assets.enqueue("assets/key.png");
-        assets.enqueue("assets/Notethumbnail.png");
-        assets.enqueue("assets/walls/wall1.png");
-        assets.enqueue("assets/walls/wall2.png");
-        assets.enqueue("assets/walls/wall3.png");
-        assets.enqueue("assets/walls/wall4.png");
-        assets.enqueue("assets/walls/wall5.png");
-        assets.enqueue("assets/walls/wall6.png");
-        assets.enqueue("assets/walls/wall8.png");
-        assets.enqueue("assets/walls/wall9.png");
-        assets.enqueue("assets/walls/wall10.png");
-        assets.enqueue("assets/walls/wall12.png");
-        assets.enqueue("assets/walls/wall14.png");
+        assets.enqueue("packed_assets/atlas.xml");
+        assets.enqueue("packed_assets/textures.png");
 		
         assets.loadQueue(function onProgress(ratio:Int) {
 		
